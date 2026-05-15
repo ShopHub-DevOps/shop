72c1add7ad454e93e5b0c8ddda20fd37eba68c37
Đorđe Ristić <djordjer88@gmail.com>
2026-05-15 22:05:46 +0200

    fix: increase e2e timeout and use Testcontainers for database

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -3,17 +3,31 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import {
+  PostgreSqlContainer,
+  StartedPostgreSqlContainer,
+} from '@testcontainers/postgresql';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
+  let container: StartedPostgreSqlContainer;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    container = await new PostgreSqlContainer('postgres:16-alpine').start();
+
+    process.env.DATABASE_URL = container.getConnectionUri();
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+    await container.stop();
   });
 
   it('/ (GET)', () => {
@@ -28,9 +42,5 @@ describe('AppController (e2e)', () => {
       .get('/health')
       .expect(200)
       .expect({ status: 'ok' });
-  });
-
-  afterEach(async () => {
-    await app.close();
   });
 });
